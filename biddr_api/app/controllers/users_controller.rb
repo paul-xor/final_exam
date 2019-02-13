d63d38d5bc88257ce01faf222cf4a7397f9cb231
Pavel Smolov <palexsmol@gmail.com>
2019-02-12 23:25:17 -0800

    sign up users from UI

---
+++ biddr_api/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+def new 
+    @user = User.new
+end
+def create
+    # debugger
+    @user = User.new(user_params)
+
+    if @user.save
+        #session[:user_id] = @user.id
+        flash[:success] = "Welcome to the blog #{@user.username}"
+        redirect_to auctions_path
+    else
+        render :new
+    end
+end
+
+private
+
+def user_params
+    params.require(:user).permit(:username, :email, :password)
+end
+
+
+end
--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :bids, only: [:create, :destroy]
   end
   get("/", to: "welcome#index", as: :root)
-  
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
 
 end
